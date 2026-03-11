f8f32cbd16d7ae565324daddc38a3dafa3428369
nzaramba <rithanzaramba@gmail.com>
2026-03-11 23:06:17 +0000

    first work shop challenge

---
+++ index.js
@@ -0,0 +1,58 @@
+// let itemprice = 25;
+// let tax = 5;
+// let total = itemprice + tax;
+// console.log(total);
+// // substraction
+// let sub = itemprice + tax;
+// console.log(sub);
+// // //multiplication 
+
+// let mul = itemprice * tax;
+// console.log(mul);
+// // //division
+// let division = itemprice / tax;
+// console.log(division);
+// //power
+// let pow= itemprice **tax;
+// console.log(pow);
+
+// let mod= itemprice/tax;
+// console.log(mod);
+// let rem=10%2;
+// console.log(rem);
+// let flourWeight=500
+// let sugarWeight=200
+// let totalWeight=flourWeight+sugarWeight;
+// let totalWeight1=totalWeight/3;
+// let leftGrams=totalWeight1%3;
+// console.log(totalWeight+"g");
+// console.log(totalWeight1);
+
+// console.log(leftGrams)
+// let bankBalance=1000;
+// bankBalance+=500;
+// bankBalance-=150;
+// bankBalance*=2;
+// console.log("The remainig balance is "+bankBalance);
+// let a =22 ;
+// let b ="22"
+// const eq= a== b 
+// console.log(eq)
+// let userAge=""
+// // let userAge=21;
+// // console.log(userAge===21);
+// // console.log(userAge>=18);
+
+// // console.log(userAge!==13);
+// console.log("enter your age "+userAge);
+let cartTotal=50;
+let isPremiumMember=true
+let hasDiscountedCode=true;
+let isFraudulent=false;
+console.log( (cartTotal>100 || isPremiumMember )&& !isFraudulent);
+// console.log(!isFraudulent);
+
+
+
+
+
---
+++ workshop.js
@@ -0,0 +1,12 @@
+console.log("Semester 1 Grades are follow")
+let physics=80;
+physics++
+let mathematics=65;
+mathematics+=10;
+let computer=30;
+console.log("physics marks are "+physics);
+console.log("mathematics with added bonus are "+mathematics);
+console.log(physics>computer)
+console.log("you failed computer")
+console.log("Are major courses passed :"+ (physics && mathematics >70))
+
