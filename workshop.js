80d2cd9e5619c8248630dcb4c86f6052117e31d4
nzaramba <rithanzaramba@gmail.com>
2026-03-12 22:44:53 +0000

    Implement operations in javascript

--- workshop.js
+++ workshop.js
@@ -1,12 +1,107 @@
-console.log("Semester 1 Grades are follow")
-let physics=80;
-physics++
-let mathematics=65;
-mathematics+=10;
-let computer=30;
-console.log("physics marks are "+physics);
-console.log("mathematics with added bonus are "+mathematics);
-console.log(physics>computer)
-console.log("you failed computer")
-console.log("Are major courses passed :"+ (physics && mathematics >70))
+// console.log("Semester 1 Grades are follow")
+// let physics=80;
+// physics++
+// let mathematics=65;
+// mathematics+=10;
+// let computer=30;
+// console.log("physics marks are "+physics);
+// console.log("mathematics with added bonus are "+mathematics);
+// console.log(physics>computer)
+// console.log("you failed computer")
+// console.log("Are major courses passed :"+ (physics && mathematics >70))
+
+//Bus fare eligibikity
+let ticketPrice=500
+let userBalance=1000
+let userAge = 15
+let ticketPayment=userBalance-ticketPrice;
+console.log("the payment made is "+ticketPayment)
+console.log("Boarding allowed " + (userAge>12 && userBalance>=500 && ticketPayment >= ticketPrice) );
+// Challenge 1: Bus Fare Eligibility
+// let ticketPrice = 500;
+// let userBalance = 600;
+// let userAge = 15;
+
+// let canPay = userBalance >= ticketPrice && userAge > 12;
+// console.log("User can board: " + canPay);
+
+// //online store discount
+// let maize=25
+// let banana=25
+// let orange=25
+// let isMember=true
+// let totalPrice=maize+banana+orange;
+// let discountapplies=totalPrice>100 && isMember
+// console.log("The total price"+totalPrice+"$")
+// console.log("Discount applies ?"+discountapplies)
+// let discount=totalPrice*0.10
+// let finalprice=totalPrice-discount
+// console.log("the final price is "+finalprice)
+// // Student pass /fail system
+// let passingScore=50
+// let distinction=85
+// let attendance=74
+// console.log("will student pass"+passingScore>=50 && attendance>=75)
+// console.log("Is student in disticntion"+distinction>=85)
+// //Phone Battery
+// let batteryPercentage=50
+// let batteryUsage=batteryPercentage--
+// let isChargeConnected=true
+// console.log("This battery is in use"+batteryUsage)
+// let warning=batteryPercentage<20 || !isChargeConnected
+// console.log("Warning please charge ypur phone"+ warning)
+// //cinema Ticket Pricing
+// let adultTicket=10
+// let chileTicket=5
+// let totalTickets=adultTicket+chileTicket
+// let isWeekend=true
+// let discounts=(totalTickets>5 && isWeekend + totalTickets*0.15)
+// console.log("the totla ticket price"+totalTickets+"rfw")
+// console.log("when discount applied"+discounts)
+// //Gym Access system
+// let hasMembership=true
+// let age=17
+// let gymCapacity=40
+// let maximumCapacity=50
+// console.log("Can user enter"+hasMembership && age>=16 && gymCapacity<maximumCapacity)
+// //INternet data usage
+// let monthlyLimit=50
+// let usage=20
+// let isPremium=true
+// let speed=0.5*100
+// let speedreduced=speed--
+// let remainingData=monthlyLimit-usage
+// console.log("remainig data"+remainingData+"GB")
+// console.log("speed reduction"+speedreduced)
+// //Game Level UNlock system
+// let score=2000
+// let lives=3
+// let coinsCollected=30
+// console.log("you are qualified ?"+score>=1000 && lives>0&&coinsCollected>=50)
+// //Food Delivry fee calculatro
+// let baseDelivryFee=2000
+// let burger=2500
+// let pizza=3000
+// let orderAmount=2000
+// let deliveryPremium=true
+// let totalOrderPrice=burger+pizza
+// freeDelivery=totalOrderPrice>1500 || deliveryPremium
+// console.log("the total order amount"+totalOrderPrice)
+// console.log("You have free delivery"+ freeDelivery)
+// //Event entry system
+// let eventTicket=30
+// let hasIdVerfication=true
+// let eventAge=21
+// let hasTicket=true
+// let pass=hasTicket&&eventTicket===30
+// console.log("User can enter"+pass && hasIdVerfication)
+
+
+
+
+
+
+
+
+
 
